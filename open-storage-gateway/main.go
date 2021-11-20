94c6b1e5f692dc49fab4a5d64d78e849bbd2fd5f
Nico Schett <52858351+schettn@users.noreply.github.com>
2021-11-20 20:28:00 +0000

    feat: add storage endpoints
    Add the file upload and download endpoints to the storage service.

---
+++ open-storage-gateway/main.go
@@ -0,0 +1,95 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"strings"
+
+	"github.com/gin-gonic/gin"
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+)
+
+func main() {
+
+	token := os.Getenv("TELEGRAM_TOKEN")
+
+	// Check if token is set else throw error with message
+	if token == "" {
+		log.Fatal("TELEGRAM_TOKEN is not set")
+	}
+
+	bot, err := tgbotapi.NewBotAPI(token)
+
+	if err != nil {
+		log.Panic(err)
+	}
+
+	bot.Debug = true
+
+	log.Printf("Authorized on account %s", bot.Self.UserName)
+
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+
+	r.GET("/storage/:id", func(c *gin.Context) {
+		id := c.Params.ByName("id")
+
+		directUrl, err := bot.GetFileDirectURL(id)
+
+		fmt.Print(directUrl)
+
+		if err != nil {
+			c.AbortWithStatus(http.StatusNotFound)
+			return
+		}
+
+		// download and serve directUrl
+		resp, err := http.Get(directUrl)
+
+		if err != nil {
+			c.AbortWithStatus(http.StatusNotFound)
+			return
+		}
+
+		contentLength := resp.ContentLength
+		contentType := resp.Header.Get("Content-Type")
+
+		// get filename from directUrl
+		fileName := directUrl[strings.LastIndex(directUrl, "/")+1:]
+
+		extraHeaders := map[string]string{
+			"Content-Disposition": fmt.Sprintf("attachment; filename=%s", fileName),
+		}
+
+		defer resp.Body.Close()
+
+		c.DataFromReader(http.StatusOK, contentLength, contentType, resp.Body, extraHeaders)
+	})
+
+	// Upload file to storage
+	r.POST("/storage", func(c *gin.Context) {
+		fileHeader, _ := c.FormFile("file")
+		file, _ := fileHeader.Open()
+		bytes, _ := ioutil.ReadAll(file)
+
+		upload_bytes := tgbotapi.FileBytes{Name: fileHeader.Filename, Bytes: bytes}
+
+		msg := tgbotapi.NewDocument(-761029330, upload_bytes)
+		ret, err := bot.Send(msg)
+
+		if err != nil {
+			log.Panic(err)
+		}
+
+		c.JSON(200, ret.Document)
+	})
+
+	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+}
