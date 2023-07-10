d88f1fd6e92583288d832993c2fe08c335fe33d0
Nico Schett <52858351+schettn@users.noreply.github.com>
2023-07-10 13:39:39 +0200

    Switched to self-hosted Telegram API server
    Implemented the following additional features provided by the self-hosted variant of the Telegram API server, as described in the official Telegram Bot API documentation:
    
    1. File Downloads: Updated the code to directly download files from the local server instead of relying on the cloud-based Telegram API. This ensures faster and more efficient file transfers.
    2. Local File Path: Modified the code to utilize the local file path for file operations, allowing for direct access and manipulation of files on the server.
    By leveraging these features, the web server now offers improved performance, reduced reliance on external services, and increased control over file handling operations.
    
    Please note that these changes require setting up and running the self-hosted Telegram API server as described in the Telegram Bot API documentation.
    
    ref: https://core.telegram.org/bots/api#using-a-local-bot-api-server

--- main.go
+++ main.go
@@ -1,125 +1,111 @@
 package main
 
 import (
-	"bytes"
-	"fmt"
-	"io/ioutil"
-	"log"
-	"net/http"
-	"os"
-	"strconv"
-	"strings"
+        "bytes"
+        "fmt"
+        "io/ioutil"
+        "log"
+        "net/http"
+        "os"
+        "strconv"
+        "path/filepath"
 
-	"github.com/gin-contrib/cors"
-	"github.com/gin-gonic/gin"
-	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+        "github.com/gin-contrib/cors"
+        "github.com/gin-gonic/gin"
+        tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 )
 
 func main() {
 
-	token := os.Getenv("TELEGRAM_TOKEN")
-	chatId := os.Getenv("TELEGRAM_CHAT_ID")
+        token := os.Getenv("TELEGRAM_TOKEN")
+        chatId := os.Getenv("TELEGRAM_CHAT_ID")
 
-	// Check if token is set else throw error with message
-	if token == "" {
-		log.Fatal("TELEGRAM_TOKEN is not set")
-	}
+        // Check if token is set else throw error with message
+        if token == "" {
+                log.Fatal("TELEGRAM_TOKEN is not set")
+        }
 
-	// Check if chatId is set else throw error with message
-	if chatId == "" {
-		log.Fatal("TELEGRAM_CHAT_ID is not set")
-	}
+        // Check if chatId is set else throw error with message
+        if chatId == "" {
+                log.Fatal("TELEGRAM_CHAT_ID is not set")
+        }
 
-	bot, err := tgbotapi.NewBotAPI(token)
+        apiURL := "http://localhost:8081/bot%s/%s"
+        bot, err := tgbotapi.NewBotAPIWithAPIEndpoint(token, apiURL)
+        //bot, err := tgbotapi.NewBotAPI(token)
+        if err != nil {
+                log.Panic(err)
+        }
 
-	if err != nil {
-		log.Panic(err)
-	}
+        bot.Debug = true
 
-	bot.Debug = true
+        log.Printf("Authorized on account %s", bot.Self.UserName)
 
-	log.Printf("Authorized on account %s", bot.Self.UserName)
+        r := gin.Default()
 
-	r := gin.Default()
+        r.Use(cors.New(cors.Config{
+                AllowAllOrigins: true,
+        }))
 
-	r.Use(cors.New(cors.Config{
-		AllowAllOrigins: true,
-	}))
+        r.GET("/ping", func(c *gin.Context) {
+                c.JSON(200, gin.H{
+                        "message": "pong",
+                })
+        })
 
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
+        r.GET("/storage/:id", func(c *gin.Context) {
+                fileID := c.Param("id")
+                file, err := bot.GetFile(tgbotapi.FileConfig{FileID: fileID})
+                if err != nil {
+                        log.Fatal(err)
+                        c.AbortWithStatus(http.StatusNotFound)
+                        return
+                }
 
-	r.GET("/storage/:id", func(c *gin.Context) {
-		id := c.Params.ByName("id")
+                localFilePath := file.FilePath
 
-		directUrl, err := bot.GetFileDirectURL(id)
+                fileData, err := ioutil.ReadFile(localFilePath)
+                if err != nil {
 
-		if err != nil {
-			c.AbortWithStatus(http.StatusNotFound)
-			return
-		}
+                        c.AbortWithStatus(http.StatusInternalServerError)
+                        return
+                }
 
-		// download and serve directUrl
-		resp, err := http.Get(directUrl)
+                contentLength := int64(len(fileData))
+                contentType := http.DetectContentType(fileData)
+                fileName := filepath.Base(localFilePath)
 
-		if err != nil {
-			c.AbortWithStatus(http.StatusNotFound)
-			return
-		}
+                extraHeaders := map[string]string{
+                        "Content-Disposition": fmt.Sprintf("attachment; filename=%s", fileName),
+                }
 
-		contentLength := resp.ContentLength
+                c.DataFromReader(http.StatusOK, contentLength, contentType, bytes.NewReader(fileData), extraHeaders)
+        })
+        // Upload file to storage
+        r.POST("/storage", func(c *gin.Context) {
+                fileHeader, err := c.FormFile("file")
 
-		// get filename from directUrl
-		fileName := directUrl[strings.LastIndex(directUrl, "/")+1:]
+                if err != nil {
+                        c.AbortWithStatus(http.StatusBadRequest)
+                        return
+                }
 
-		extraHeaders := map[string]string{
-			"Content-Disposition": fmt.Sprintf("attachment; filename=%s", fileName),
-		}
+                file, _ := fileHeader.Open()
+                bytes, _ := ioutil.ReadAll(file)
 
-		defer resp.Body.Close()
+                upload_bytes := tgbotapi.FileBytes{Name: fileHeader.Filename, Bytes: bytes}
 
-		// Read the content
-		var bodyBytes []byte
-		if c.Request.Body != nil {
-			bodyBytes, _ = ioutil.ReadAll(resp.Body)
-		}
-		// Restore the io.ReadCloser to its original state
-		resp.Body = ioutil.NopCloser(bytes.NewBuffer(bodyBytes))
+                chatId, _ := strconv.ParseInt(chatId, 10, 64)
 
-		// Use the content to specify the mimeType
-		contentType := http.DetectContentType(bodyBytes)
+                msg := tgbotapi.NewDocument(chatId, upload_bytes)
+                ret, err := bot.Send(msg)
 
-		c.DataFromReader(http.StatusOK, contentLength, contentType, resp.Body, extraHeaders)
-	})
+                if err != nil {
+                        log.Panic(err)
+                }
 
-	// Upload file to storage
-	r.POST("/storage", func(c *gin.Context) {
-		fileHeader, err := c.FormFile("file")
+                c.JSON(200, ret.Document)
+        })
 
-		if err != nil {
-			c.AbortWithStatus(http.StatusBadRequest)
-			return
-		}
-
-		file, _ := fileHeader.Open()
-		bytes, _ := ioutil.ReadAll(file)
-
-		upload_bytes := tgbotapi.FileBytes{Name: fileHeader.Filename, Bytes: bytes}
-
-		chatId, _ := strconv.ParseInt(chatId, 10, 64)
-
-		msg := tgbotapi.NewDocument(chatId, upload_bytes)
-		ret, err := bot.Send(msg)
-
-		if err != nil {
-			log.Panic(err)
-		}
-
-		c.JSON(200, ret.Document)
-	})
-
-	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+        r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
